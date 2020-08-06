27d19a1202a59ddef275e61d0aba199a80314f6a
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-06 12:34:03 +0100

    mounting data from options successfully completed

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -4,15 +4,18 @@ import axios from 'axios'
 class GamePage extends React.Component {
   state = {
     quotes: '',
-    character: ''
+    character: '',
+    options: []
   }
 
   async componentDidMount() {
     try {
-      const res = await axios.get('https://cors-anywhere.herokuapp.com/https://officeapi.dev/api/quotes/random')
-
-      this.setState({ quotes: res.data.data.content, character: res.data.data.character.firstname })
-      console.log(res.data)
+      // const resQuote = await axios.get('https://cors-anywhere.herokuapp.com/https://officeapi.dev/api/quotes/random')
+      const resChar = await axios.get('https://cors-anywhere.herokuapp.com/https://officeapi.dev/api/characters/')
+      this.setState({ options: resChar.data.data })
+      console.log(resChar.data.data)
+      // this.setState({ quotes: resQuote.data.data.content, character: resQuote.data.data.character.firstname })
+      // console.log(resQuote.data)
     } catch (err) {
       console.log(err)
     }
@@ -23,7 +26,11 @@ class GamePage extends React.Component {
       <>
         <h5>Quote: </h5>
         <h4>' {this.state.quotes} '</h4>
-        <h4> {this.state.character}</h4>
+        {/* <h4> {this.state.character}</h4> */}
+        <div>{this.state.options.map(option => (
+          <p key={option._id}>{option.firstname}</p>
+        ))}
+        </div>
       </>
     )
   }
