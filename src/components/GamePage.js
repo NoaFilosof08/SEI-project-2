d6a50ec73408e52208316106e96dbcfdb70938ba
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-06 11:26:25 +0100

    basic components for home and game page set up

---
+++ src/components/GamePage.js
@@ -0,0 +1,9 @@
+import React from 'react' 
+
+const GamePage = () => {
+  return (
+    <h1>GAME</h1>
+  )
+}
+
+export default GamePage
---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <>
+      <h1>Dwitanic</h1>
+      <button>
+        <Link to="/game">Click me!</Link>
+      </button>
+    </>
+  )
+}
+
+export default Home
