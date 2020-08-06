244fc989ca4a6e3a0f712ec846286a4f743628e7
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-06 11:56:59 +0100

    api data for quote successfully mounted on page

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -1,9 +1,32 @@
 import React from 'react' 
+import axios from 'axios'
 
-const GamePage = () => {
-  return (
-    <h1>GAME</h1>
-  )
+class GamePage extends React.Component {
+  state = {
+    quotes: '',
+    character: ''
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await axios.get('https://cors-anywhere.herokuapp.com/https://officeapi.dev/api/quotes/random')
+
+      this.setState({ quotes: res.data.data.content, character: res.data.data.character.firstname })
+      console.log(res.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h5>Quote: </h5>
+        <h4>' {this.state.quotes} '</h4>
+        <h4> {this.state.character}</h4>
+      </>
+    )
+  }
 }
 
 export default GamePage
