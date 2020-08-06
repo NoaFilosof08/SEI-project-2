4b2c021fa5f22a9f44236b9999ce1a18841414c4
NoaFilosof <noafilosof@hotmail.co.uk>
2020-08-06 17:31:35 +0100

    random name generator complete

--- src/components/GamePage.js
+++ src/components/GamePage.js
@@ -6,7 +6,7 @@ class GamePage extends React.Component {
     quotes: '',
     character: '',
     options: [],
-    amendedOption: []
+    amendedOption: null 
   }
 
   async componentDidMount() {
@@ -16,29 +16,38 @@ class GamePage extends React.Component {
       this.setState({ options: resChar.data.data })
       console.log(resChar.data.data)
       this.setState({ quotes: resQuote.data.data.content, character: resQuote.data.data.character.firstname })
-
-      const random = this.state.options[Math.floor(Math.random() * this.state.options.length)]
-      this.setState({ amendedOption: random.firstname })
+      this.getRandom()
     } catch (err) {
       console.log(err)
     }
   }
 
+  getRandom = () => {
+    let currentChoices = []
+    for (let i = 0; i < 3; i++) {
+      const random = this.state.options[Math.floor(Math.random() * this.state.options.length)]
+      if (random.firstname === this.state.character) {
+        return this.getRandom()
+      } else {
+        currentChoices.push(random.firstname)
+      }
+    }  
+    console.log(currentChoices)
+    this.setState({ amendedOption: [...currentChoices] })
+  }
 
   render() {
+    if (!this.state.amendedOption) return null 
     console.log(this.state.amendedOption)
     return (
       <>
         <h5>Quote: </h5>
         <h4>' {this.state.quotes} '</h4>
-        {/* <h4>Right answer: </h4> */}
-        {/* <div>{this.state.options.map(option => (
-          <p key={option._id}>{option.firstname}</p>
-        ))}
-        </div> */}
         <h6>Options:  </h6>
         <div>
-          <button>{this.state.amendedOption}</button>
+          {this.state.amendedOption.map(name => {
+            return <button key={name} value={name}>{name}</button>
+          })}
           <button>{this.state.character}</button>
         </div>
       </>
